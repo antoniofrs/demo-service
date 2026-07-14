4f212f80cdd46868dfdbdc0eaa2abd94758984fc
alex <anassisi@studenti.apuliadigital.it>
2026-07-14 16:24:55 +0200

    collegamento

--- src/main/java/it/its/demo/demo_service/model/Book.java
+++ src/main/java/it/its/demo/demo_service/model/Book.java
@@ -1,17 +1,23 @@
 package it.its.demo.demo_service.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.context.annotation.Primary;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class Book {
+    @Id
     String id;
     String name;
     String author;
     Integer quantity;
+    String description;
     Double price;
 }
 
